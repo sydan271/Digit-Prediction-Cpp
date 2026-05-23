e890345d217d19264ab5fe612078142680706cba
Dan Truong <dansytruong@gmail.com>
2026-05-22 22:01:07 -0500

    setting up the header files

---
+++ neuralNetwork/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "readDataset.h"
+#include "matrixHelper.h"
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <cmath>
+#include <random>
+#include <chrono>
+#include "neuralNetwork.h"
+int main()
+{
+
+    return 0;
+}
---
+++ neuralNetwork/neuralNetwork.h
@@ -0,0 +1,15 @@
+#ifndef NEURALNETWORK_H
+#define NEURALNETWORK_H
+
+#include <iostream>
+#include <vector>
+#include <random>
+
+class NeuralNetwork
+{
+    
+
+};
+
+
+#endif // NEURALNETWORK_H
